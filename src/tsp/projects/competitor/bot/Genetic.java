a02f0a94c9af3aa2a395a28bcc4232177847a099
AlexandreU <46602409+ALEX67U@users.noreply.github.com>
2024-02-27 00:13:06 +0100

    Mutation opt

--- src/tsp/projects/competitor/bot/Genetic.java
+++ src/tsp/projects/competitor/bot/Genetic.java
@@ -12,8 +12,8 @@ public class Genetic extends CompetitorProject
 {
     private int length;
     private Random random;
-    private int N = 100;
-    private static double MUTATION = 0.05;
+    private int N = 50;
+    private final double MUTATION = 0.5;
 
     ArrayList<Path> population = new ArrayList<Path>();
 
@@ -62,14 +62,14 @@ public class Genetic extends CompetitorProject
         this.random = new Random();
         this.length = this.problem.getLength ();
 
-        Path path = HillClimbing(this.length);
-        this.population.add(path);
-        for (int i = 1; i < this.N/2  ; i++)
+        Path path;
+        for (int i = 0; i < 2*this.N/3  ; i++)
         {
             path = two_opt(HillClimbing(this.length));
             this.population.add(path);
         }
-        for (int i = this.N/2; i < this.N  ; i++)
+
+        for (int i = 2*this.N/3; i < this.N  ; i++)
         {
             path = two_opt(new Path(this.length));
             this.population.add(path);
@@ -97,19 +97,13 @@ public class Genetic extends CompetitorProject
             Coordinates i2 = this.problem.getCoordinates(S.getPath()[l]);
 
             int n = l + 1;
-            int m = n + 1;
             for (int j = i ; j < length; j++)
             {
-                if (n >= length)
-                {
-                    n = 0;
-                    m = 1;
-                }
+                if (n >= length) n = 0;
 
-                if (m >= length)
-                {
-                    m = 0;
-                }
+                int m = n + 1;
+
+                if (m >= length) m = 0;
 
                 Coordinates j1 = this.problem.getCoordinates(S.getPath()[n]);
                 Coordinates j2 = this.problem.getCoordinates(S.getPath()[m]);
@@ -124,60 +118,98 @@ public class Genetic extends CompetitorProject
                     ameliorable = true;
                     invercePath(S, l ,n );
                     i2 = this.problem.getCoordinates(S.getPath()[l]);
-                    /*j1 = this.problem.getCoordinates(S.getPath()[n]);
-                    j2 = this.problem.getCoordinates(S.getPath()[m]);
-
-                    dist1 = i1.distance(i2);
-                    dist2 = j1.distance(j2);
-                    dist3 = i2.distance(j2);
-                    dist4 = i1.distance(j1);
-
-                    double oo = this.evaluation.evaluate(P);
-                    double ff = this.evaluation.evaluate(S);
-
-                    if((dist1 + dist2) > (dist3 + dist4))
-                    {
-                        System.err.println("test  : " + (dist1 + dist2) + " < " + (dist3 + dist4));
-                    }
-
-                    if(oo < ff)
-                    {
-                        System.err.println(oo + " < " + ff);
-                        System.err.println(P);
-                        System.err.println(S);
-                    }*/
                 }
 
                 n = n + 1;
-                m = n + 1;
             }
         }
+        return S;
+    }
+
+    Path new_opt(Path P) {
+        Path S = new Path(P);
+        int max = 0;
+        boolean ameliorable = true;
+
+        while (ameliorable && max < length)
+            for (int i = 0; i < length; i++)
+            {
+                ameliorable = false;
+                Coordinates i1 = this.problem.getCoordinates(S.getPath()[i]);
+
+                int l = i + 1;
+
+                if (l == length) l = 0;
+
+                int k = l + 1;
+
+                if (k == length) k = 0;
+
+                Coordinates i2 = this.problem.getCoordinates(S.getPath()[l]);
+                Coordinates i3 = this.problem.getCoordinates(S.getPath()[k]);
+
+                int n = k + 1;
+                for (int j = 0; j < length - 3; j++)
+                {
+                    if (n >= length) n = 0;
+
+                    int m = n + 1;
+
+                    if (m >= length) m = 0;
+
+                    Coordinates j1 = this.problem.getCoordinates(S.getPath()[n]);
+                    Coordinates j2 = this.problem.getCoordinates(S.getPath()[m]);
+
+                    double dist1 = i1.distance(i2); // i à i+1
+                    double dist2 = i2.distance(i3); // i+1 à i+2
+                    double dist3 = j1.distance(j2); // j à i+1
+
+                    double dist4 = j1.distance(i2); // j à i+1
+                    double dist5 = i2.distance(j2); // i+1 à j+1
+                    double dist6 = i1.distance(i3); // i à i+2
+
+                    if ((dist1 + dist2 + dist3) > (dist4 + dist5 + dist6))
+                    {
+                        ameliorable = true;
+                        shiftPath(S, l ,n );
+                        i1 = this.problem.getCoordinates(S.getPath()[i]);
+                        i2 = this.problem.getCoordinates(S.getPath()[l]);
+                        i3 = this.problem.getCoordinates(S.getPath()[k]);
+                    }
+                    n++;
+                }
+                max++;
+            }
         return S;
     }
 
     public void sortList()
     {
         Collections.sort(this.population, Comparator.comparingDouble(this.evaluation::evaluate));
-
     }
 
     @Override
     public void loop ()
     {
-        for (int i = 10; i < this.N  - 1 ; i++)
+        for (int i = this.N/10; i < this.N ; i++)
         {
-            int index1 = random.nextInt(100);
+            int index1 = random.nextInt(this.N/2);
             int index2;
             do {
-                index2 = random.nextInt(100);
+                index2 =  random.nextInt(this.N/2);
             } while (index2 == index1);
 
             Path path1 = population.get(index1);
             Path path2 = population.get(index2);
             int rand = random.nextInt(this.length);
-            Path pathc1 = Children(path1,path2,rand);
+            Path pathc = two_opt(Children(path1,path2,rand));
 
-            this.population.set(i,two_opt(pathc1));
+            if (random.nextDouble(1) < this.MUTATION)
+            {
+                pathc = new_opt(pathc);
+            }
+
+            this.population.set(i,pathc);
         }
         this.sortList();
     }
@@ -250,12 +282,50 @@ public class Genetic extends CompetitorProject
 
     }
 
+    void MutationShift(Path P)
+    {
+        int index1 = randomIndex();
+        int index2 = randomIndex(index1);
+        shiftPath(P,index1,index2);
+    }
+
+    void shiftPath(Path P,int index1,int index2)
+    {
+        int[] p = P.getPath();
+        int tmp;
+        int n = index1;
+        int length = (index2 - index1 );
+
+        if(index1 > index2)
+        {
+            length = ((this.length  - index1) + index2);
+        }
+        for (int i = 0 ; i < length  ; i++)
+        {
+            if (n == this.length)n = 0;
+
+            int m = n + 1;
+
+            if (m == this.length) m = 0;
+
+            tmp = p[n];
+            p[n] = p[m];
+            p[m] = tmp;
+
+            m++;
+            n++;
+        }
+
+    }
+
     void MutationInvertion(Path P)
     {
         int index1 = randomIndex();
         int index2 = randomIndex(index1);
         invercePath(P,index1,index2);
     }
+
+
 
     void MutationSwap(Path P)
     {
